936555419708aa6ba3d2405a702e0c9e3a8036c5
Benjamin Kober <benko@kober-systems.com>
2023-09-25 11:53:13 +0200

    Added tests for a multi delimiter protocol.

--- test/native/test_io/delimiter_framed_io.cpp
+++ test/native/test_io/delimiter_framed_io.cpp
@@ -9,6 +9,13 @@
 #define STR_SZ(string) sizeof(string)-1
 #define STR(string) string, STR_SZ(string)
 
+DelimiterFramedIO::parse_state multi_delimiter(char cur, size_t pos) {
+  if (cur == '\n' || cur == ';') {
+    return DelimiterFramedIO::parse_state::found;
+  }
+  return DelimiterFramedIO::parse_state::not_found;
+}
+
 SCENARIO("Delimiter Protocol") {
   char buffer[BUFFER_SZ];
   auto usb = FakeIO();
@@ -35,7 +42,7 @@ SCENARIO("Delimiter Protocol") {
     }}}}
   }
 
-    GIVEN("We have a newline delimited protocol") {
+  GIVEN("We have a newline delimited protocol") {
     auto proto = DelimiterFramedIO(&usb, '\n');
 
     WHEN("a string is send but multiple newlines") {
@@ -56,6 +63,57 @@ SCENARIO("Delimiter Protocol") {
     }}}}
   }
 
+  GIVEN("We have a delimited protocol with multiple delimiters: \n and ;") {
+    auto proto = DelimiterFramedIO(&usb, &multi_delimiter);
+
+    WHEN("a string is send but without any delimiter") {
+      usb.simulate_send(STR("my test string"));
+
+    THEN("no data is received yet") {
+      int recv_bytes = proto.read(buffer, BUFFER_SZ);
+
+      REQUIRE(recv_bytes == 0);
+
+    WHEN("a newline is submitted") {
+      usb.simulate_send(STR(".\n"));
+
+    THEN("it receives the string till the newline") {
+      recv_bytes = proto.read(buffer, BUFFER_SZ);
+
+      CHECK(recv_bytes == STR_SZ("my test string.\n"));
+      CHECK(std::string(buffer, recv_bytes) == "my test string.\n");
+
+    WHEN("a string with semicolon is submitted") {
+      usb.simulate_send(STR("Another message;"));
+
+    THEN("it receives the string till the semicolon") {
+      recv_bytes = proto.read(buffer, BUFFER_SZ);
+
+      CHECK(recv_bytes == STR_SZ("Another message;"));
+      CHECK(std::string(buffer, recv_bytes) == "Another message;");
+    }}}}}}
+  }
+
+  GIVEN("We have a delimited protocol with multiple delimiters: \n and ;") {
+    auto proto = DelimiterFramedIO(&usb, &multi_delimiter);
+
+    WHEN("a string is send but multiple delimiters") {
+      usb.simulate_send(STR("my test;xstring\n"));
+
+    THEN("we only receive the first string") {
+      int recv_bytes = proto.read(buffer, BUFFER_SZ);
+
+      CHECK(recv_bytes == STR_SZ("my test;"));
+      CHECK(std::string(buffer, recv_bytes) == "my test;");
+
+    WHEN("we try to receive another message") {
+      recv_bytes = proto.read(buffer, BUFFER_SZ);
+
+    THEN("it receives the string till the next delimiter") {
+      CHECK(recv_bytes == STR_SZ("xstring\n"));
+      CHECK(std::string(buffer, recv_bytes) == "xstring\n");
+    }}}}
+  }
 }
 
 int main(int argc, char **argv)
