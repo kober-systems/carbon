4c36d8780271bf09922e511b929a815b1018d491
Benjamin Kober <benko@kober-systems.com>
2023-10-10 13:50:20 +0200

    Problem: Could not use library as dependency under windows
    Reason: Windows git implementation does not support symbolic links
    Solution: Use a build script wich sets the include paths dynamically

--- scripts/monorepo.py
+++ scripts/monorepo.py
@@ -1,3 +1,12 @@
+# import all sublibraries into one monorepo like repo
 Import("env")
 
-print(env.Dump())
+import os
+from os import path
+
+PATH_PREFIX=path.join(env["PROJECT_LIBDEPS_DIR"], env["PIOENV"], "carbon", "libraries")
+
+env.Append(
+  CPPPATH=[path.join(PATH_PREFIX, p) for p in os.listdir(PATH_PREFIX)]
+)
+
